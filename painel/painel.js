2516f755d7375ea2971e437a6e9c676a054bbc7f
rafflores17-prog <rafflores17@gmail.com>
2026-03-29 08:50:39 -0300

    Update painel.js

--- painel/painel.js
+++ painel/painel.js
@@ -1,21 +1,15 @@
 function gerarLink() {
     const url = document.getElementById('urlInput').value;
-    if(!url) return alert("Insira uma URL válida!");
-
-    // Codifica a URL em Base64 para passar pelo parâmetro 'd'
-    const encoded = btoa(url);
-    
-    // Define a rota do site principal (ajusta automaticamente no Vercel)
+    if(!url) return alert("Insira um link!");
+    const encoded = btoa(url); 
     const baseUrl = window.location.origin + "/site/index.html";
     const finalLink = `${baseUrl}?d=${encoded}`;
-
     document.getElementById('resultBox').style.display = "block";
     document.getElementById('outputLink').value = finalLink;
 }
-
 function copiar() {
     const copyText = document.getElementById("outputLink");
     copyText.select();
     document.execCommand("copy");
-    alert("Link copiado para a área de transferência!");
+    alert("Copiado com sucesso!");
 }
